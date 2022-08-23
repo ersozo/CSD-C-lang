472fb2772dbeb02d30a5754820ca8f3a474fec6c
Ozgur Ersoz <ozgur.ersoz3@gmail.com>
2022-08-23 23:08:30 +0300

    update

--- q1.c
+++ q1.c
@@ -1,69 +1,68 @@
-// Online C compiler to run C program online
-#include <stdio.h>
+// #include <stdio.h>
 
-int pattern_odd(int x)
-{
-    int i, j;
+// int pattern_odd(int x)
+// {
+//     int i, j;
 
-    for (i = 1; i <= x; i++)
-    {
-        putchar('|');
-        if (i <= x)
-        {
-            for (j = 1; j <= i - 1; j++)
-                putchar('-');
-            putchar('*');
-            for (j = 1; j <= x - i; j++)
-                putchar('-');
-        }
-        printf("|\n");
-    }
-}
+//     for (i = 1; i <= x; i++)
+//     {
+//         putchar('|');
+//         if (i <= x)
+//         {
+//             for (j = 1; j <= i - 1; j++)
+//                 putchar('-');
+//             putchar('*');
+//             for (j = 1; j <= x - i; j++)
+//                 putchar('-');
+//         }
+//         printf("|\n");
+//     }
+// }
 
-int pattern_even(int x)
-{
-    int i, j;
-    for (i = 1; i <= x - 1; i++)
-    {
-        putchar('|');
-        if (i <= x)
-        {
-            for (j = 1; j <= x - i - 1; j++)
-                putchar('-');
-            putchar('*');
+// int pattern_even(int x)
+// {
+//     int i, j;
+//     for (i = 1; i <= x - 1; i++)
+//     {
+//         putchar('|');
+//         if (i <= x)
+//         {
+//             for (j = 1; j <= x - i - 1; j++)
+//                 putchar('-');
+//             putchar('*');
 
-            for (j = 1; j <= i; j++)
-                putchar('-');
-        }
-        printf("|\n");
-    }
-}
+//             for (j = 1; j <= i; j++)
+//                 putchar('-');
+//         }
+//         printf("|\n");
+//     }
+// }
 
-int main()
-{
-    int h, w;
-    printf("enter the height: ");
-    scanf("%d", &h);
-    printf("enter the width: ");
-    scanf("%d", &w);
+// int main()
+// {
+//     int h, w;
+//     printf("enter the height: ");
+//     scanf("%d", &h);
+//     printf("enter the width: ");
+//     scanf("%d", &w);
 
-    if (w <= h)
-    {
-        int counter = 1;
-        while (counter <= h / w)
-        {
-            if (counter % 2 == 0)
-                pattern_even(w);
-            else
-                pattern_odd(w);
-            counter++;
-        }
-    }
-    else
-        printf("width cannot be greater than height!");
+//     if (w <= h)
+//     {
+//         int counter = 1;
+//         while (counter <= h / w)
+//         {
+//             if (counter % 2 == 0)
+//                 pattern_even(w);
+//             else
+//                 pattern_odd(w);
+//             counter++;
+//         }
+//     }
+//     else
+//         printf("width cannot be greater than height!");
 
-    return 0;
-}
+//     return 0;
+// }
 
 // solution 2
 #include <stdio.h>
