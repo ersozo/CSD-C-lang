cff70ebe61e95862c0e7ab8a5c234088ab93cdaa
ozgur <ozgur.ersoz3@gmail.com>
2022-08-23 17:31:25 +0300

    typo and q1 update

--- q1.c
+++ q1.c
@@ -1,8 +1,75 @@
+// Online C compiler to run C program online
 #include <stdio.h>
-int main(void)
 
+int pattern_odd(int x)
 {
+    int i, j;
 
+    for (i = 1; i <= x; i++)
+    {
+        putchar('|');
+        if (i <= x)
+        {
+            for (j = 1; j <= i - 1; j++)
+                putchar('-');
+            putchar('*');
+            for (j = 1; j <= x - i; j++)
+                putchar('-');
+        }
+        printf("|\n");
+    }
+}
+
+int pattern_even(int x)
+{
+    int i, j;
+    for (i = 1; i <= x - 1; i++)
+    {
+        putchar('|');
+        if (i <= x)
+        {
+            for (j = 1; j <= x - i - 1; j++)
+                putchar('-');
+            putchar('*');
+
+            for (j = 1; j <= i; j++)
+                putchar('-');
+        }
+        printf("|\n");
+    }
+}
+
+int main()
+{
+    int h, w;
+    printf("enter the height: ");
+    scanf("%d", &h);
+    printf("enter the width: ");
+    scanf("%d", &w);
+
+    if (w <= h)
+    {
+        int counter = 1;
+        while (counter <= h / w)
+        {
+            if (counter % 2 == 0)
+                pattern_even(w);
+            else
+                pattern_odd(w);
+            counter++;
+        }
+    }
+    else
+        printf("width cannot be greater than height!");
+
+    return 0;
+}
+
+// solution 2
+#include <stdio.h>
+
+int main()
+{
     int h, w;
     printf("enter the height: ");
     scanf("%d", &h);
@@ -14,30 +81,26 @@ int main(void)
     for (i = 1; i <= h; i++)
     {
         putchar('|');
+        printf("%d", i);
+        if (i <= w)
+        {
+            for (j = 1; j <= i - 1; j++)
+                putchar('-');
+            putchar('*');
+            for (j = 1; j <= w - i; j++)
+                putchar('-');
+        }
+        else
+        {
+            for (j = 1; j <= 2 * w - i - 1; j++)
+                putchar('-');
+            putchar('*');
 
-        for (j = 1; j <= i; j++)
-            putchar(' ');
-        putchar('*');
-
-        for (j = 1; j <= w - i; j++)
-            putchar(' ');
-
-        printf("|\n");
-    }
-
-    for (i = 1; i <= h; i++)
-    {
-        putchar('|');
-
-        for (j = 1; j <= w - i; j++)
-            putchar(' ');
-        putchar('*');
-
-        for (j = 1; j <= i; j++)
-            putchar(' ');
+            for (j = 1; j <= i - w; j++)
+                putchar('-');
+        }
 
         printf("|\n");
     }
-
     return 0;
 }
